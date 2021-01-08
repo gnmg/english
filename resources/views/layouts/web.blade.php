ac273efd54a5a42c2a4af648892807f8edec1b98
gnmg <ralph@ralphreene.info>
2021-01-08 13:43:12 +0900

    help

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -21,7 +21,7 @@
     <!-- <link rel="stylesheet" href="/public/css/normalize.css">
     <link rel="stylesheet" href="/public/css/style.css">
     <!-- Style file for validation tooltip -->
-    <link rel="stylesheet" href="/public/css/validationEngine.css"> -->
+    <link rel="stylesheet" href="/public/css/validationEngine.css">
 
     <link rel="stylesheet" href="public/css/normalize.css">
     <link rel="stylesheet" href="public/css/style.css">
